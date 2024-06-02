7dde60fe7b588e1d4fcc87449ccbd3cc6da2a4ea
danik <kotsovskijdanilo@gmail.com>
2024-06-02 12:34:28 +0300

    class point

--- task/task.cpp
+++ task/task.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class point {
+    int x, y;
+public:
+    point() :x(0), y(0) {}
+    point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    void print()const {
+        cout << "X: " << x << " , Y: " << y << "\n";
+    }
+};
+
 int main()
 {
     
